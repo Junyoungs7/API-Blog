6c6140ac5f8a2769fcf5d2299dc138c669d1e42f
junyoungs7 <rlawnsdud98@gmail.com>
2022-11-03 20:06:14 +0900

    22-11-03 : OpenApi 받아옴

--- src/main/java/com/jun/blog/weatherDomain/controller/WeatherController.java
+++ src/main/java/com/jun/blog/weatherDomain/controller/WeatherController.java
@@ -1,6 +1,8 @@
 package com.jun.blog.weatherDomain.controller;
 
+import com.jun.blog.weatherDomain.service.WeatherApiService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,9 +11,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RequestMapping("/weather")
 public class WeatherController {
+    private final WeatherApiService weatherApiService;
 
     @GetMapping("/weathers")
     public String getWeather(){
-        return "hi";
+        return weatherApiService.getApi();
     }
 }
--- src/main/java/com/jun/blog/weatherDomain/service/WeatherApiService.java
+++ src/main/java/com/jun/blog/weatherDomain/service/WeatherApiService.java
@@ -1,13 +1,43 @@
 package com.jun.blog.weatherDomain.service;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.util.DefaultUriBuilderFactory;
 
 @Service
 public class WeatherApiService {
+    @Value("${weather-apikey}")
+    String serviceKey;
+    String BASE_URL = "http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getVilageFcst";
+    public String getApi(){
+        String pageNo = "1";
+        String numOfRows = "1000";
+        String dataType = "JSON";
+        String base_date = "20221103";
+        String base_time = "0500";
+        String  nx = "55";
+        String ny = "127";
+        DefaultUriBuilderFactory factory = new DefaultUriBuilderFactory(BASE_URL);
+        factory.setEncodingMode(DefaultUriBuilderFactory.EncodingMode.VALUES_ONLY);
+        WebClient webClient = WebClient.builder().uriBuilderFactory(factory).baseUrl(BASE_URL).build();
 
-    public void getApi(){
-        DefaultUriBuilderFactory factory = new DefaultUriBuilderFactory()
+        String response = webClient.get()
+                .uri(uriBuilder -> uriBuilder
+                        .queryParam("serviceKey", serviceKey)
+                        .queryParam("pageNo", pageNo)
+                        .queryParam("numOfRows", numOfRows)
+                        .queryParam("dataType", dataType)
+                        .queryParam("base_date", base_date)
+                        .queryParam("base_time", base_time)
+                        .queryParam("nx", nx)
+                        .queryParam("ny", ny)
+                        .build())
+                .retrieve()
+                .bodyToMono(String.class)
+                .block();
+
+        return response;
     }
 
 }
