107df129c4de927fab46fa6473f07310c3188073
junyoungs7 <rlawnsdud98@gmail.com>
2022-11-03 16:14:11 +0900

    fixed untracked files

---
+++ src/main/java/com/jun/blog/weatherDomain/service/WeatherApiService.java
@@ -0,0 +1,13 @@
+package com.jun.blog.weatherDomain.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.util.DefaultUriBuilderFactory;
+
+@Service
+public class WeatherApiService {
+
+    public void getApi(){
+        DefaultUriBuilderFactory factory = new DefaultUriBuilderFactory()
+    }
+
+}
